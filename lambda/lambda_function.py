503a31243eac645fc66708b30163ba796f7b1d59
Sanket <sanketn767@gmail.com>
2025-01-01 16:58:32 +0530

    change deploy final

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,6 +1,6 @@
- import json
+import json
 
- def lambda_handler(event,context):
+def lambda_handler(event,context):
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda test visual studio code next try final!')
